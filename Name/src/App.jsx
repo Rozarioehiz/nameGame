fbaacd0bce41ff0d1d8baf5af924ed0852745484
Rozarioehiz <rozarioehiz@gmail.com>
2024-01-25 09:32:39 -0800

    first commit

---
+++ Name/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  const[state,setState]=useState("")
+
+  const handlechange =()=>{
+
+
+  const email ="rose"
+
+  if(email !== state){
+    alert("wrong email")
+  }
+  else{
+    alert("enjoy")
+  }
+}
+
+  return (
+   <div className='body'>
+    <p >Enter name to guess</p>
+   <input type="text" 
+   placeholder='Name'
+   onChange={(e)=>setState(e.target.value)}     
+   />
+   <button onClick={handlechange}>check</button>
+   </div>
+  )
+}
+
+export default App
+
